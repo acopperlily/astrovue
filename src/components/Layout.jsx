3aa786156fd4b6e827cbb9ec3dc989bb96006c39
acopperlily <april.copley@protonmail.com>
2023-03-11 10:01:46 -0500

    Implement fetch

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import Footer from "./Footer";
 
 const Layout = () => {
+  const [data, setData] = useState({})
+
+  const handleFetch = e => {
+    console.log('e', e.target.value);
+    const key = import.meta.env.VITE_API_KEY;
+    let date = e.target.value;
+    let url = `https://api.nasa.gov/planetary/apod?api_key=${key}&date=${date}`;
+
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data)
+        setData(data);
+      })
+      .catch(err => {
+        console.log('error:', err);
+      })
+
+  }
+
   return (
     <div className="site-wrapper">
       <Header />
       <main>
-        <Outlet />
+        <Outlet context={{ data, handleFetch}} />
       </main>
       <Footer />
     </div>
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useOutletContext } from "react-router-dom";
 
 const Search = props => {
+  const { data, handleFetch } = useOutletContext();
+  const [title, setTitle] = useState('');
+
+  console.log('wut', handleFetch);
   return (
     <div className="container">
       <h1>Search page</h1>
+      <img src={data.hdurl} />
+      {data.title}
+      {data.date}
+      {data.explanation}
+      <input 
+        className="btn" 
+        type="date" 
+        name="date" 
+        id="date" 
+        onChange={(e) => handleFetch(e)}
+      />
     </div>
   );
 };
